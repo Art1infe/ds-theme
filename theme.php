9ed352d108d4e09b009742862d62b97fb52758e3
Art1infe <ferati.art10@gmail.com>
2025-03-04 19:20:21 +0100

    car

---
+++ theme.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Theme</title>
+    <?php wp_head(); ?>
+</head>
+
+    <?php 
+        if(is_front_page()):
+            $awesome_classes = array( 'ds-class', 'my-ds-class');
+        else():
+            $awesome_classes = array( 'no-ds-class');
+        endif;
+    ?>
+
+<body <?php body_class($awesome_classes ); ?>>
+
+        <?php  wp_nav_menu(array('theme_location' => 'primary'));?>
+    
+</body>
+</html>
